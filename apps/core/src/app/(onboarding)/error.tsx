441139a13d32672bdc67d3480308734b65bdbf94
Josh Cobert <jdcobert@gmail.com>
2025-11-19 20:28:59 -0500

    escape error text

--- apps/core/src/app/(onboarding)/error.tsx
+++ apps/core/src/app/(onboarding)/error.tsx
@@ -18,7 +18,7 @@ export default function Error({
       <div className='flex flex-col gap-2 text-center max-w-prose'>
         <h2 className='text-2xl font-semibold'>Something went wrong!</h2>
         <p className='text-muted-foreground'>
-          We couldn't complete your registration. Please try again.
+          {`We couldn't complete your registration. Please try again.`}
         </p>
       </div>
       <Button onClick={() => reset()}>Try again</Button>
